e36248c518ab52ac70ee0943bb9258b9796a6bdf
dhazlettjr <d.hazlett.jr@gmail.com>
2017-11-28 10:35:07 -0600

    added contact files

---
+++ contact.js
@@ -0,0 +1,21 @@
+function clicker() {
+   // document.getElementById("alert").innerHTML = "Thank you!";
+   var hide = document.getElementById("box");
+   if (hide.style.display ==="none") {
+       hide.style.display = "block";
+
+   } else {
+       hide.style.display = "none";
+   }
+}
+ //if (document.getElementById("mail").value==="") {
+    //document.getElementById("box").style.display="block";
+//} else {
+ //   document.getElementById("box").style.display="none";
+//}
+
+function clicker1() {
+   // document.getElementById("alert1").innerHTML = "Mistake's Happen.";
+   document.getElementById("alert").innerHTML = "";
+}
+
