5e8d04db72d197b178038e73cfe49a2172a0046a
dhazlettjr <d.hazlett.jr@gmail.com>
2017-11-28 10:42:29 -0600

    made changes to index and .JS

--- contact.js
+++ contact.js
@@ -1,21 +1,9 @@
 function clicker() {
-   // document.getElementById("alert").innerHTML = "Thank you!";
-   var hide = document.getElementById("box");
-   if (hide.style.display ==="none") {
-       hide.style.display = "block";
+   document.getElementById("alert").innerHTML = "Thank you!";
 
-   } else {
-       hide.style.display = "none";
-   }
 }
- //if (document.getElementById("mail").value==="") {
-    //document.getElementById("box").style.display="block";
-//} else {
- //   document.getElementById("box").style.display="none";
-//}
 
 function clicker1() {
    // document.getElementById("alert1").innerHTML = "Mistake's Happen.";
    document.getElementById("alert").innerHTML = "";
 }
-
